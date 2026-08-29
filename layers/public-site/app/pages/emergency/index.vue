1c8289a4a9bf85c2d92bdcd9a82f186335dc461c
Siljan123 <jansilgasoc859@gmail.com>
2026-08-29 20:27:41 +0800

    ft: where-to-stay-eat

---
+++ layers/public-site/app/pages/emergency/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+definePageMeta({
+    layout:'guest'
+})
+</script>
+
+<template>
+    <UiHeroSection
+    title="Emergency & Hospitals"
+    description="Quick access to hospitals, clinics, pharmacies, and emergency hotlines in the LGU SFADS."
+    />
+
+</template>
