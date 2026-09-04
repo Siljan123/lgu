806d067bae9c8da89cc2a23f8e6cee485481f520
Siljan123 <jansilgasoc859@gmail.com>
2026-09-04 16:26:11 +0800

    feat: sdb eed change values

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,13 +6,12 @@ export default defineNuxtConfig({
     './layers/public-site'
   ],
     runtimeConfig: {
-    supabaseUrl: process.env.SUPABASE_URL || 'http://127.0.0.1:54321',
-    supabaseKey: process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0',
-    supabaseServiceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU',
+    supabaseUrl: process.env.SUPABASE_URL,
+    supabaseKey: process.env.SUPABASE_KEY ,
     public: {
       googleMapsApiKey: process.env.NUXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-      supabaseUrl: process.env.SUPABASE_URL || 'http://127.0.0.1:54321',
-      supabaseKey: process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0',
+      supabaseUrl: process.env.SUPABASE_URL ,
+      supabaseKey: process.env.SUPABASE_KEY 
     },
   },
 })
